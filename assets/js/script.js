8a9366889300849a412984c63beb21d19c02a1e7
Amer Kulovic <amer.kulovic@yahoo.com>
2022-11-29 20:53:00 -0600

    set up the search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,46 @@
 // GLOBAL VARIABLES
-
+let searchButton = document.querySelector("#search-button");
+let movieResults = document.querySelector("#movie-results");
+let singleMovie = document.querySelector("#single-movie");
 // FUNCTIONS
 
+function init() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const movieID = urlParams.get("i") || "";
+  if (movieID === "") {
+    return;
+  }
+  fetch(`http://www.omdbapi.com/?i=${movieID}&apikey=38367836`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      singleMovie.innerHTML += `<div>${data.Plot}</div>`;
+    });
+}
+function handleSearchButton() {
+  fetch("https://www.omdbapi.com/?apikey=f14ca85d&s=batman")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      data.Search.forEach((movie) => {
+        movieResults.innerHTML += `<a href="index.html?i=${movie.imdbID}">${movie.Title}</a>`;
+      });
+    });
+}
+// handle formSubmit
+// fetch https://www.omdbapi.com/?apikey=f14ca85d&s=batman
+// For each movie, innerhtml += movie-results
+// Create a link for each search results
+// <a href= "index.html?i=tt";
+
 // EVENT LISTENERS
+// init function
+// check the url to see if it contains the i query string
+// call another fetch http://www.omdbapi.com/?i=tt0372784&apikey=38367836
+// For each movie, innerhtml += movie-results
+// User clicks on the search button
+searchButton.addEventListener("click", handleSearchButton);
+
+init();
